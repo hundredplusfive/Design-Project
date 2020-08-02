4b16256e76ceb8255442fe4c0b50b98d048340c1
hundredplusfive <60305580+hundredplusfive@users.noreply.github.com>
2020-08-02 13:25:19 +0800

    Import data functionality added (#12)
    * Import data functionality added for compEdge.py
    
    Able to import .txt file generated from graph simualtor
    
    * Create hamiltonianCycle.py
    
    Features
    - import adjacency list
    - convert adjacency list into matrix
    - determines if a hamitonian circuit exist
    
    * Update compEdge.py
    
    Export includes a information on start and end points
    
    * Update hamiltonianCycle.py
    
    Hamiltonian solution considers the start point
    
    * Create dfs.py (#13)
    
    * Create dfs.py
    
    Based on https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
    
    Tweaks
    1) print solution method
    2) imports an Adjacency list from .txt file to build the graph
    
    * Update hamiltonianCycle.py
    
    Output displays "starting vertex number"
    
    * Update compEdge.py
    
    Hamiltonian circuit and DFS programs are imported inside

--- compEdge.py
+++ compEdge.py
@@ -5,6 +5,9 @@ Created on Mon Jul 20 20:00:50 2020
 
 @author: luting
 """
+import numpy as np
+import hamiltonianCycle
+import dfs
 
 class Vertice:
     def __init__(self, a, b, c):
@@ -12,11 +15,7 @@ class Vertice:
         self.x = b
         self.y = c
 
-class Edge:
-    def __init__(self, a, b):
-        self.root = a
-        self.branch = b
-
+#lattice-based 4-edge check
 def edge_check(a, b, c, d):
     if c-a == 1 and d-b == 0:
         return True
@@ -29,31 +28,40 @@ def edge_check(a, b, c, d):
     else:
         return False
 
-#insert code to open .txt to state the lattice dimension
-# ---> here    
-r = 2
-c = 2
+#import dimension, start/end details from .txt
+f = open("/home/luting/Documents/DP/processingJV/segment-0.txt",'r')
 
-#number of vertices
-noV = 0 
+arrDimSE = [] #temp array 
 
-#list of coordinate objects
-c_list = []
+for line in f.readlines():
+    arrDimSE.append(line)
 
-#row increment
-for i in range(r):
-    #column increment
-    for j in range(c):
+c, r = arrDimSE[0].split('x') #number of columns, number of rows
+
+noV = 0 #number of vertices
+
+c_list = [] #list of coordinate objects
+
+for i in range(int(r)): #row increment
+    for j in range(int(c)): #column increment
         v = Vertice(noV, j, i)
         c_list.append(v)
         noV += 1
 
 #print coordinate list
+print("\n\nVerticeID-to-Coordinate Reference")
 for obj in c_list:
     print(obj.id, obj.x, obj.y)
- 
-f = open("edge.txt", "w+")
     
+#adjancency matrix filled with zeroes
+adjMat = np.zeros([noV, noV], dtype = int)
+
+#export function    
+f = open("edge.txt", "w+")
+
+f.write(str(noV) + "\n")
+f.write(arrDimSE[1])
+
 for base in c_list:
     for target in c_list:
         #set compare all coordinate sets to each other
@@ -61,7 +69,38 @@ for base in c_list:
         ty = base.y
         #check if edge exist
         if edge_check(tx, ty, target.x, target.y) == True:
-           e = Edge(base.id, target.id)
-           f.write(str(base.id) + " -> " + str(target.id) + "\n")
+           f.write(str(base.id) + "->" + str(target.id) + "\n")
+           adjMat[base.id, target.id] = 1
 
 f.close()
+arrDimSE.clear()
+
+print("\nAdjancency Matrix of Segemented Area")        
+print(adjMat)
+
+#path planning
+f = open("edge.txt", "r")
+
+arrVerAdj = []
+
+for line in f.readlines():
+    arrVerAdj.append(line)
+    
+cc_hc = hamiltonianCycle.Graph(int(arrVerAdj[0]))
+cc_dfs = dfs.Graph()    
+
+s, e = arrVerAdj[1].split(">>")
+
+for element in arrVerAdj[2:]: #skips first two lines
+    ver, adj = element.split('->')
+    cc_hc.graph[int(ver)][int(adj)] = 1
+    cc_dfs.addEdge(int(ver),int(adj))
+
+# Print the solution
+print("\nHamiltonian cycle solution:") 
+cc_hc.hamCycle(int(s))
+
+print("Depth First Traversal solution:")
+cc_dfs.printSolution(s)       
+
+arrVerAdj.clear()
